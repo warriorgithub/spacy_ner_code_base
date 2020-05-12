d9be20e8a8501c30b1e4762f10863ecaa3732db0
Naveen Sharma <naveen.sharma@imaginea.com>
2020-05-12 15:22:48 +0530

    code corrected

--- json_to_spacy.py
+++ json_to_spacy.py
@@ -1,73 +1,45 @@
-from __future__ import unicode_literals, print_function
-import pickle
 import plac
-import random
-from pathlib import Path
-import spacy
-from spacy.util import minibatch, compounding
+import logging
+import argparse
+import sys
+import os
+import json
+import pickle
+
+@plac.annotations(input_file=("Input file", "option", "i", str), output_file=("Output file", "option", "o", str))
+
+def main(input_file=None, output_file=None):
+    try:
+        training_data = []
+        lines=[]
+        with open(input_file, 'r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            data = json.loads(line)
+            text = data['content']
+            entities = []
+            if data['annotation']!=None:
+
+                for annotation in data['annotation']:
+                    point = annotation['points'][0]
+                    labels = annotation['label']
+                    if not isinstance(labels, list):
+                        labels = [labels]
+
+                    for label in labels:
+                        entities.append((point['start'], point['end'] + 1 ,label))
 
 
-# New entity labels
-# Specify the new entity labels which you want to add here
-LABEL = ['Skill']
+                training_data.append((text, {"entities" : entities}))
 
+            print(training_data)
 
-# Loading training data 
-with open ('rskimodel', 'rb') as fp:
-    TRAIN_DATA = pickle.load(fp)
-    #print('TRAIN DATA   ------------------------           ',TRAIN_DATA)
+        with open(output_file, 'wb') as fp:
+            pickle.dump(training_data, fp)
 
-@plac.annotations(
-    model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
-    new_model_name=("New model name for model meta.", "option", "nm", str),
-    output_dir=("Optional output directory", "option", "o", Path),
-    n_iter=("Number of training iterations", "option", "n", int))
-
-def main(model=None, new_model_name='new_model', output_dir=None, n_iter=30):
-    """Setting up the pipeline and entity recognizer, and training the new entity."""
-    if model is not None:
-        nlp = spacy.load(model)  
-        print("Loaded model '%s'" % model)
-    else:
-        nlp = spacy.blank('en')  
-        print("Created blank 'en' model")
-    if 'ner' not in nlp.pipe_names:
-        ner = nlp.create_pipe('ner')
-        nlp.add_pipe(ner)
-    else:
-        ner = nlp.get_pipe('ner')
-
-    for i in LABEL:
-        ner.add_label(i)   # Add new entity labels to entity recognizer
-
-    if model is None:
-        optimizer = nlp.begin_training()
-    else:
-        optimizer = nlp.entity.create_optimizer()
-
-    # Get names of other pipes to disable them during training to train only NER
-    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
-    with nlp.disable_pipes(*other_pipes):  # only train NER
-        for itn in range(n_iter):
-            random.shuffle(TRAIN_DATA)
-            losses = {}
-            batches = minibatch(TRAIN_DATA, size=compounding(4., 32., 1.001))
-            for batch in batches:
-                texts, annotations = zip(*batch)
-                #print(texts, '  |   ',annotations)
-                nlp.update(texts, annotations, sgd=optimizer, drop=0.35,
-                           losses=losses)
-            print('Losses', losses)
-
-    # Save model 
-    if output_dir is not None:
-        output_dir = Path(output_dir)
-        if not output_dir.exists():
-            output_dir.mkdir()
-        print(new_model_name)    
-        nlp.meta['name'] = new_model_name  # rename model
-        nlp.to_disk(output_dir)
-        print("Saved model to", output_dir)        
-
+    except Exception as e:
+        logging.exception("Unable to process " + input_file + "\n" + "error = " + str(e))
+        return None
 if __name__ == '__main__':
     plac.call(main)
